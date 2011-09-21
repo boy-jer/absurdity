90bda94bf707e54ac9102ffcd5afd07e6d79f02b
Tim Payton <timpayton@gmail.com>
2011-09-21 17:45:12 +0200

    Fixed missing instance call for singleton

--- lib/absurdity/railtie.rb
+++ lib/absurdity/railtie.rb
@@ -2,7 +2,7 @@ module Absurdity
   class Railtie < ::Rails::Railtie
     config.after_initialize do
       load ::Rails.root.join("absurdity/experiments.rb")
-      Absurdity::Config.logger = ::Rails.logger
+      Absurdity::Config.instance.logger = ::Rails.logger
     end
   end
 end
